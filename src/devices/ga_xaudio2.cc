ae02c4e612b6c35d9602bf53139b903496545468
Moonchild <elronnd@elronnd.net>
2020-11-05 15:45:06 -0800

    port xaudio2 to new dev framework (untested)

--- src/devices/ga_xaudio2.cc
+++ src/devices/ga_xaudio2.cc
@@ -10,28 +10,33 @@
 #include <stdio.h>
 #include <assert.h>
 
+
+struct ga_DeviceImpl {
+	struct IXAudio2* xa;
+	struct IXAudio2MasteringVoice* master;
+	struct IXAudio2SourceVoice* source;
+	gc_int32 sampleSize;
+	gc_uint32 nextBuffer;
+	void** buffers;
+};
+
 extern "C" {
-ga_DeviceImpl_XAudio2* gaX_device_open_xaudio2(gc_int32 in_numBuffers, gc_int32 in_numSamples, ga_Format* in_format) {
-	ga_DeviceImpl_XAudio2* ret = (ga_DeviceImpl_XAudio2*)gcX_ops->allocFunc(sizeof(ga_DeviceImpl_XAudio2));
+static gc_result gaX_open(ga_Device *dev) {
 	HRESULT result;
 	WAVEFORMATEX fmt;
 	gc_int32 i;
-	ret->numBuffers = in_numBuffers;
-	ret->numSamples = in_numSamples;
-	memcpy(&ret->format, in_format, sizeof(ga_Format));
-	ret->sampleSize = ga_format_sampleSize(in_format);
-	ret->nextBuffer = 0;
-	ret->xa = 0;
-	ret->master = 0;
+	dev->impl = gcX_ops->allocFunc(sizeof(gaX_DeviceImpl));
+	dev->impl->sampleSize = ga_format_sampleSize(&dev->format);
+	dev->impl->nextBuffer = 0;
+	dev->impl->xa = 0;
+	dev->impl->master = 0;
 
 	CoInitializeEx(NULL, COINIT_MULTITHREADED);
-	result = XAudio2Create(&ret->xa, 0, XAUDIO2_DEFAULT_PROCESSOR);
-	if(FAILED(result))
-		goto cleanup;
+	result = XAudio2Create(&dev->impl->xa, 0, XAUDIO2_DEFAULT_PROCESSOR);
+	if (FAILED(result)) goto cleanup;
 
-	result = ret->xa->CreateMasteringVoice(&ret->master, 2, 44100, 0, 0, 0);
-	if(FAILED(result))
-		goto cleanup;
+	result = dev->impl->xa->CreateMasteringVoice(&dev->impl->master, 2, 44100, 0, 0, 0);
+	if (FAILED(result)) goto cleanup;
 
 	fmt.cbSize = sizeof(WAVEFORMATEX);
 	ZeroMemory(&fmt, sizeof(WAVEFORMATEX));
@@ -43,83 +48,75 @@ ga_DeviceImpl_XAudio2* gaX_device_open_xaudio2(gc_int32 in_numBuffers, gc_int32 
 	fmt.nBlockAlign = fmt.nChannels * (fmt.wBitsPerSample / 8);
 	fmt.nAvgBytesPerSec = fmt.nSamplesPerSec * fmt.nBlockAlign;
 
-	result = ret->xa->CreateSourceVoice(&ret->source, &fmt, XAUDIO2_VOICE_NOPITCH, XAUDIO2_DEFAULT_FREQ_RATIO, 0, 0, 0);
-	if(FAILED(result))
-		goto cleanup;
+	result = dev->impl->xa->CreateSourceVoice(&dev->impl->source, &fmt, XAUDIO2_VOICE_NOPITCH, XAUDIO2_DEFAULT_FREQ_RATIO, 0, 0, 0);
+	if (FAILED(result)) goto cleanup;
 
-	result = ret->xa->StartEngine();
-	if(FAILED(result))
-		goto cleanup;
+	result = dev->impl->xa->StartEngine();
+	if (FAILED(result)) goto cleanup;
 
-	result = ret->source->Start(0, XAUDIO2_COMMIT_NOW);
-	if(FAILED(result))
-		goto cleanup;
+	result = dev->impl->source->Start(0, XAUDIO2_COMMIT_NOW);
+	if (FAILED(result)) goto cleanup;
 
-	ret->buffers = (void**)gcX_ops->allocFunc(ret->numBuffers * sizeof(void*));
-	for(i = 0; i < ret->numBuffers; ++i)
-		ret->buffers[i] = gcX_ops->allocFunc(ret->numSamples * ret->sampleSize);
+	dev->impl->buffers = (void**)gcX_ops->allocFunc(dev->num_buffers * sizeof(void*));
+	for(i = 0; i < dev->num_buffers; ++i)
+		dev->impl->buffers[i] = gcX_ops->allocFunc(dev->num_samples * dev->impl->sampleSize);
 
-	return ret;
+	return GC_SUCCESS;
 
 cleanup:
-	if(ret->source)
-	{
-		ret->source->Stop(0, XAUDIO2_COMMIT_NOW);
-		ret->source->FlushSourceBuffers();
-		ret->source->DestroyVoice();
+	if (dev->impl->source) {
+		dev->impl->source->Stop(0, XAUDIO2_COMMIT_NOW);
+		dev->impl->source->FlushSourceBuffers();
+		dev->impl->source->DestroyVoice();
 	}
-	if(ret->xa)
-		ret->xa->StopEngine();
-	if(ret->master)
-		ret->master->DestroyVoice();
-	if(ret->xa)
-		ret->xa->Release();
+	if (dev->impl->xa) dev->impl->xa->StopEngine();
+	if (dev->impl->master) dev->impl->master->DestroyVoice();
+	if (dev->impl->xa) dev->impl->xa->Release();
 	CoUninitialize();
-	gcX_ops->freeFunc(ret);
-	return 0;
+	gcX_ops->freeFunc(dev->impl);
+	return GC_ERROR_GENERIC;
 }
 
-gc_result gaX_device_close_xaudio2(ga_DeviceImpl_XAudio2* in_device) {
+static gc_result gaX_close(ga_Device *dev) {
 	gc_int32 i;
-	if(in_device->source) {
-		in_device->source->Stop(0, XAUDIO2_COMMIT_NOW);
-		in_device->source->FlushSourceBuffers();
-		in_device->source->DestroyVoice();
+	if(dev->impl->source) {
+		dev->impl->source->Stop(0, XAUDIO2_COMMIT_NOW);
+		dev->impl->source->FlushSourceBuffers();
+		dev->impl->source->DestroyVoice();
 	}
-	if(in_device->xa)
-		in_device->xa->StopEngine();
-	if(in_device->master)
-		in_device->master->DestroyVoice();
-	if(in_device->xa)
-		in_device->xa->Release();
+	if(dev->impl->xa)
+		dev->impl->xa->StopEngine();
+	if(dev->impl->master)
+		dev->impl->master->DestroyVoice();
+	if(dev->impl->xa)
+		dev->impl->xa->Release();
 	CoUninitialize();
 
-	for(i = 0; i < in_device->numBuffers; ++i)
-		gcX_ops->freeFunc(in_device->buffers[i]);
+	for(i = 0; i < dev->num_buffers; ++i)
+		gcX_ops->freeFunc(dev->impl->buffers[i]);
 	gcX_ops->freeFunc(in_device->buffers);
 	gcX_ops->freeFunc(in_device);
 	return GC_SUCCESS;
 }
 
-gc_int32 gaX_device_check_xaudio2(ga_DeviceImpl_XAudio2* in_device) {
+static gc_int32 gaX_check(ga_Device *dev) {
 	gc_int32 ret = 0;
 	XAUDIO2_VOICE_STATE state = { 0 };
-	in_device->source->GetState(&state);
-	ret = in_device->numBuffers - state.BuffersQueued;
-	return ret;
+	dev->impl->source->GetState(&state);
+	return dev->num_buffers - state.BuffersQueued;
 }
 
-gc_result gaX_device_queue_xaudio2(ga_DeviceImpl_XAudio2* in_device, void* in_buffer) {
+static gc_result gaX_queue(ga_Device *dev, void *in_buffer) {
 	XAUDIO2_BUFFER buf;
 	void* data;
 	ZeroMemory(&buf, sizeof(XAUDIO2_BUFFER));
-	buf.AudioBytes = in_device->numSamples * in_device->sampleSize;
-	data = in_device->buffers[in_device->nextBuffer++];
-	in_device->nextBuffer %= in_device->numBuffers;
+	buf.AudioBytes = dev->num_samples * dev->impl->sampleSize;
+	data = dev->impl->buffers[in_device->nextBuffer++];
+	dev->impl->nextBuffer %= dev->num_buffers;
 	memcpy(data, in_buffer, buf.AudioBytes);
 	buf.pAudioData = (const BYTE*)data;
-	in_device->source->SubmitSourceBuffer(&buf, 0);
+	dev->impl->source->SubmitSourceBuffer(&buf, 0);
 	return GC_SUCCESS;
 }
-
+gaX_DeviceProcs gaX_deviceprocs_XAudio2 = { gaX_open, gaX_check, gaX_queue, gaX_close };
 } // extern "C"
