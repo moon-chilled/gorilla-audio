f1d86b85ba29e92a1f93e08809f107c07cdbe264
Moonchild <elronnd@elronnd.net>
2021-02-10 23:03:18 -0800

    oops

---
+++ src/ga/devices/sndio.c
@@ -0,0 +1,60 @@
+#include "gorilla/ga.h"
+#include "gorilla/ga_internal.h"
+
+#include <sndio.h>
+
+struct GaXDeviceImpl {
+	struct sio_hdl *hdl;
+};
+
+static ga_result gaX_open(GaDevice *dev) {
+	dev->impl = ga_alloc(sizeof(GaXDeviceImpl));
+	if (!dev->impl) return GA_ERR_SYS_MEM;
+	dev->impl->hdl = sio_open(SIO_DEVANY, SIO_PLAY, 1/*async*/);
+	if (!dev->impl->hdl) goto fail;
+
+	struct sio_par par;
+       	sio_initpar(&par);
+	par.bits = dev->format.bits_per_sample;
+	par.bps = dev->format.bits_per_sample >> 3;
+	par.sig = dev->format.bits_per_sample != 8;
+	par.le = ENDIAN(1, 0);
+	par.pchan = dev->format.num_channels;
+	par.rate = dev->format.sample_rate;
+	par.appbufsz = dev->num_buffers * dev->num_samples;
+	par.round = dev->num_samples;
+	par.xrun = SIO_SYNC;
+	if (!sio_setpar(dev->impl->hdl, &par)) goto fail;
+	if (par.le != ENDIAN(1, 0)
+	    || par.bps != par.bits >> 3
+	    || par.sig != (dev->format.bits_per_sample != 8)) goto fail;
+	dev->format.bits_per_sample = par.bits;
+	dev->format.num_channels = par.pchan;
+	dev->format.sample_rate = par.rate;
+	dev->num_samples = par.round;
+	dev->num_buffers = par.appbufsz / par.round;
+
+	return GA_OK;
+
+fail:
+	if (dev->impl->hdl) sio_close(dev->impl->hdl);
+	ga_free(dev->impl);
+	return GA_ERR_SYS_LIB;
+}
+
+static ga_result gaX_close(GaDevice *dev) {
+	sio_close(dev->impl->hdl);
+	ga_free(dev->impl);
+	return GA_OK;
+}
+
+static u32 gaX_check(GaDevice *dev) {
+	return dev->num_buffers; //TODO
+}
+
+static ga_result gaX_queue(GaDevice *dev, void *buf) {
+	if (sio_write(dev->impl->hdl, buf, ga_format_sample_size(&dev->format) * dev->num_samples) != ga_format_sample_size(&dev->format) * dev->num_samples) return GA_ERR_SYS_LIB;
+	return GA_OK;
+}
+
+GaXDeviceProcs gaX_deviceprocs_sndio = { gaX_open, gaX_check, gaX_queue, gaX_close };
